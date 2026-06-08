7b12d7f0194036d97f0868bb0e20631c3c312285
ngounmalin123-lang <ngounmalin123@gmail.com>
2026-06-08 11:24:07 +0700

    update portfolio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from "./components/Footer";
